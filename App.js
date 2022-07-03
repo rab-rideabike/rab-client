104c82365eb7261ddf19ff40e3978b11dad4e927
satyasuryavenkat@gmail.com <satyasuryavenkat@gmail.com>
2022-07-03 16:33:16 +0530

    provider

--- App.js
+++ App.js
@@ -7,11 +7,14 @@ import Signup from "./src/components/Signup";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { customTheme } from "./src/theme";
+import { Provider } from 'react-redux';
+import store from "./src/store";
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
+    <Provider store={store}>
     <NativeBaseProvider theme={customTheme}>
       <StatusBar animated={true} />
           <NavigationContainer>
@@ -22,5 +25,6 @@ export default function App() {
             </Stack.Navigator>
           </NavigationContainer>
     </NativeBaseProvider>
+    </Provider>
   );
 }
